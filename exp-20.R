c5aac53c4266d4ba17d6a5eecf551e2011535aee
jagadeesh9347 <128576182+jagadeesh9347@users.noreply.github.com>
2023-08-28 17:23:47 +0530

    Create exp-20.R

---
+++ exp-20.R
@@ -0,0 +1,6 @@
+# Create a new empty plot without specifying axes limits
+plot.new()
+
+# Create a new empty plot with specified axes limits
+plot.new()
+plot.window(xlim = c(0, 10), ylim = c(0, 20))
