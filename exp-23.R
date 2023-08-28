f4047aac0aaa879b035474cc90a022d4fb77c250
jagadeesh9347 <128576182+jagadeesh9347@users.noreply.github.com>
2023-08-28 17:25:38 +0530

    Create exp-23.R

---
+++ exp-23.R
@@ -0,0 +1,9 @@
+# Create data for the array
+data <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+# Create the array with dimensions (rows, columns, tables)
+array_dim <- c(3, 4, 2)
+my_array <- array(data, dim = array_dim)
+
+# Display the content of the array
+print(my_array)
